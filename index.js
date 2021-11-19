244aeb4b66f07dcaeb7be8c4e7388206927778dd
shamim1620 <mdshamimahmed2025@gmail.com>
2021-11-20 00:06:01 +0600

    port update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const app = express()
 const cors = require('cors');
-const port = process.env.PORT | 5000
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors());
